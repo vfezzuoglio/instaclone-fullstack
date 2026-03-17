0f9b9078998f16c501ae20535bf8d395a061fb3d
Vinny <vincent.fezzuoglio@protonmail.com>
2026-03-16 20:39:36 -0400

    updated env

--- instagram-clone-front/services/firebase.js
+++ instagram-clone-front/services/firebase.js
@@ -6,27 +6,13 @@ import {
   initializeAuth,
 } from "firebase/auth";
 
-const fallbackConfig = {
-  apiKey: "AIzaSyA55EVxcHcHAtQNWLhGBRcirHr3Tf0zahk",
-  authDomain: "instaclone-617e2.firebaseapp.com",
-  projectId: "instaclone-617e2",
-  storageBucket: "instaclone-617e2.firebasestorage.app",
-  messagingSenderId: "641719532054",
-  appId: "1:641719532054:web:f254c71bd8998a8c3c1337",
-};
-
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || fallbackConfig.apiKey,
-  authDomain:
-    process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || fallbackConfig.authDomain,
-  projectId:
-    process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || fallbackConfig.projectId,
-  storageBucket:
-    process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || fallbackConfig.storageBucket,
-  messagingSenderId:
-    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
-    fallbackConfig.messagingSenderId,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || fallbackConfig.appId,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 if (!firebaseConfig.apiKey) {
